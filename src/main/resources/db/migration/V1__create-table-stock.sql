6d9342d4e595ba869e3b6ad52a38087cb2f5b4f5
Júlia Freitas <juliafreitas@alu.ufc.br>
2024-02-19 23:44:36 -0300

    Use VARCHAR2 instead of VARCHAR.

--- src/main/resources/db/migration/V1__create-table-stock.sql
+++ src/main/resources/db/migration/V1__create-table-stock.sql
@@ -1,6 +1,6 @@
 CREATE TABLE stock(
-                      id VARCHAR (255) PRIMARY KEY,
-                      symbol VARCHAR (3)  NOT NULL,
-                      company_name VARCHAR (255) NOT NULL,
+                      id VARCHAR2 (255) PRIMARY KEY,
+                      symbol VARCHAR2 (3)  NOT NULL,
+                      company_name VARCHAR2 (255) NOT NULL,
                       price FLOAT NOT NULL
 );
