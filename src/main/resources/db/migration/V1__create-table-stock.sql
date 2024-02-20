2605d37dc2b8de5e88453b3feeea86091757b7fb
Júlia Freitas <juliafreitas@alu.ufc.br>
2024-02-19 23:42:16 -0300

    Add the mandatory size constraint.

--- src/main/resources/db/migration/V1__create-table-stock.sql
+++ src/main/resources/db/migration/V1__create-table-stock.sql
@@ -1,6 +1,6 @@
 CREATE TABLE stock(
-                      id VARCHAR PRIMARY KEY,
-                      symbol VARCHAR NOT NULL,
-                      company_name VARCHAR NOT NULL,
+                      id VARCHAR (255) PRIMARY KEY,
+                      symbol VARCHAR (3)  NOT NULL,
+                      company_name VARCHAR (255) NOT NULL,
                       price FLOAT NOT NULL
 );
